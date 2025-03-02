e89ee737dcd0f931d0ea10f3777413d0b9e2aec7
jeansuh42 <jeansuh42@gmail.com>
2025-03-02 18:00:46 +0900

    project init

---
+++ src/main/java/com/codingtest/genesislab/GenesislabApplication.java
@@ -0,0 +1,13 @@
+package com.codingtest.genesislab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesislabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenesislabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codingtest/genesislab/GenesislabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingtest.genesislab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesislabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
